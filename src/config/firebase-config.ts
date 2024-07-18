cefae7ccacdb95e71f0d1426234ddcbd6a7f7fcf
scorcism <abhishekpathak1720@gmail.com>
2024-07-18 22:22:01 +0530

    add firestore

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,3 +13,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export { app };
+
+const db = getFirestore(app);
+
+export { db };
