7643b8459544fb0bd12ed72c547d841951fdd6c2
Dashon <i@dashon.co>
2024-08-21 10:53:47 -0500

    fix merge error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import "./styles/main.css";
+import Layout from "./components/Layout";
+import Category from "./views/Category";
+import Document from "./views/Document";
+import BrushSquare from "./views/BrushSquare";
+import FolderAdd from "./views/FolderAdd";
+import Message from "./views/Message";
+import Settings from "./views/Settings";
+import Profile from "./views/Profile";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import Layout from "./components/Layout";
-import "./styles/main.css";
-import Home from "./views/Home";
-
 function App() {
   return (
     <div>
@@ -16,12 +22,19 @@ function App() {
         redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URL as string}
       >
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-          </Route>
-
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="category" element={<Category />}></Route>
+            <Route path="document" element={<Document />}></Route>
+            <Route path="copy" element={<Home />}></Route>
+            <Route path="brush-square" element={<BrushSquare />}></Route>
+            <Route path="folder-add" element={<FolderAdd />}></Route>
+            <Route path="message" element={<Message />}></Route>
+            <Route path="settings" element={<Settings />}></Route>
+            <Route path="profile" element={<Profile />}></Route>
+          </Route>
         </Routes>
       </KindeProvider>
     </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -130,7 +130,7 @@ function Navigation() {
             >
               <CloseCircle />
             </button>
-            <Profile setCustomerFile={() => {}} />
+            <Profile />
           </div>
         </Drawer>
       </div>
